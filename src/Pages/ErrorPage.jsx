81d3e89a1f7bd881a147d59e192b734374e489b9
Ajmain Fayek <fayekajmain0@gmail.com>
2024-11-04 03:07:00 +0600

    useRouteError Hook implemented to console error & error page is more meaningful

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+    const error = useRouteError();
+    console.error(error);
     return (
         <section className="flex items-center h-full p-16">
             <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
@@ -13,12 +15,11 @@ const ErrorPage = () => {
                         Sorry, we couldn't find this page.
                     </p>
                     <p className="mt-4 mb-8 text-gray-600">
-                        But dont worry, you can find plenty of other things on
-                        our homepage.
+                        {error.data}
                     </p>
                     <Link
                         rel="noopener noreferrer"
-                        to='/'
+                        to="/"
                         className="px-8 py-3 font-semibold rounded bg-color-pirmary text-gray-50"
                     >
                         Back to homepage
